039210e3897ff137a70475c591648834d69b9a5e
Heneli Kailahi <heneli@tvisioninsights.com>
2019-02-25 08:24:28 -0500

    Euterpea.IO.Audio.CSound - some type signatures

--- Euterpea/IO/Audio/CSound.hs
+++ Euterpea/IO/Audio/CSound.hs
@@ -5,9 +5,15 @@
 -- which used many csound names for the basic signal functions.
 module Euterpea.IO.Audio.CSound where
 
+import Control.Arrow (ArrowChoice)
+import Control.Arrow.Operations (ArrowCircuit)
+
+import Control.Arrow.ArrowP (ArrowP)
+import Control.SF.SF (SF)
 import Euterpea.IO.Audio.Basics (outA)
 import Euterpea.IO.Audio.BasicSigFuns
-  ( delayLine
+  ( Table
+  , delayLine
   , delayLine1
   , delayLineT
   , envASR
@@ -45,6 +51,7 @@ import Euterpea.IO.Audio.BasicSigFuns
   , tableSines3N
   , tableSinesN
   )
+import Euterpea.IO.Audio.Types (Clock, Signal)
 
 gen05    = tableExponN
 gen05'   = tableExpon
@@ -78,64 +85,107 @@ butterbp = filterBandPassBW
 butterbr = filterBandStopBW
 comb     = filterComb
 
+oscil :: (Clock p, ArrowCircuit a) => Table -> Double -> ArrowP a p Double Double
 oscil    = osc
+
+oscili :: (Clock p, ArrowCircuit a) => Table -> Double -> ArrowP a p Double Double
 oscili   = oscI
+
+oscils :: (Clock p, ArrowCircuit a) => Double -> ArrowP a p Double Double
 oscils f = proc a -> do
             o <- oscFixed f -< ()
-            outA -< o*a
+            outA -< o * a
+
+oscil1 :: (Clock p, ArrowChoice a, ArrowCircuit a)
+  => Table
+  -> Double
+  -> Double
+  -> ArrowP a p Double Double
 oscil1 tab del dur =
           proc a -> do
             o <- oscDur  tab del dur -< ()
             outA -< o*a
+
+oscil1i :: (Clock p, ArrowChoice a, ArrowCircuit a)
+  => Table
+  -> Double
+  -> Double
+  -> ArrowP a p Double Double
 oscil1i tab del dur =
           proc a -> do
             o <- oscDurI tab del dur -< ()
             outA -< o*a
 
+buzz :: Clock p
+  => Table
+  -> Double
+  -> Signal p (Double, Int) Double
 buzz     = oscPartials
 
 -- pluck    = pluck
 -- balance  = balance
 
+line :: Clock p => Double -> Double -> Double -> ArrowP SF p Double Double
 line a d b =
   proc s -> do
     o <- envLine a d b -< ()
     outA -< o*s
 
+expon :: Clock p => Double -> Double -> Double -> ArrowP SF p Double Double
 expon a d b =
   proc s -> do
     o <- envExpon a d b -< ()
     outA -< o*s
 
+linseg, expseg :: Clock p => [Double] -> [Double] -> Signal p () Double
 linseg   = envLineSeg
 expseg   = envExponSeg
 
+linen :: Clock p => Double -> Double -> Double -> ArrowP SF p Double Double
 linen rise dur dec =
   proc s -> do
     o <- envASR rise dur dec -< ()
     outA -< o*s
 
+envlpx :: Clock p =>
+     Double
+  -> Double
+  -> Double
+  -> Table
+  -> Double
+  -> Double
+  -> ArrowP SF p Double Double
 envlpx rise dur dec tab atss atdec =
   proc s -> do
     o <- envCSEnvlpx rise dur dec tab atss atdec -< ()
     outA -< o*s
 
+rand :: Int -> ArrowP SF p Double Double
 rand s =
   proc a -> do
     o <- noiseWhite s -< ()
     outA -< o*a
 
+randi :: Clock p => Int -> ArrowP SF p (Double, Double) Double
 randi s =
   proc (a,f) -> do
     o <- noiseBLI s -< f
     outA -< o*a
 
+randh :: Clock p => Int -> ArrowP SF p (Double, Double) Double
 randh s =
   proc (a,f) -> do
     o <- noiseBLH s -< f
     outA -< o*a
 
+delay :: Clock p => Double -> Signal p Double Double
 delay  = delayLine
+
+vdelay :: Clock p => Double -> Signal p (Double, Double) Double
 vdelay = delayLine1
+
+delay1 :: Clock p => Double -> Signal p (Double, Double) Double
 delay1 = delayLine1
+
+delayT :: Clock p => Int -> Table -> Signal p Double Double
 delayT = delayLineT
