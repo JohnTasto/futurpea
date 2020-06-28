1546e1e0b9b32c05856ce77d63fc01166dee61a7
John Tasto <5026892+JohnTasto@users.noreply.github.com>
2020-06-28 09:23:22 -0700

    Simplify ArrowP and SF arrows

--- Control/Arrow/ArrowP.hs
+++ Control/Arrow/ArrowP.hs
@@ -7,23 +7,43 @@ import Control.Arrow.Operations (ArrowCircuit (delay))
 import Control.Category (Category (id, (.)))
 
 
-newtype ArrowP a p b c = ArrowP { strip :: a b c }
+-- newtype ArrowP a p b c = ArrowP { strip :: a b c }
+
+newtype ArrowP a p b c = ArrowP (a b c)
+
+strip :: ArrowP a p b c -> a b c
+strip (ArrowP x) = x
+
 
 instance Category a => Category (ArrowP a p) where
+--id :: ArrowP a p a1 a1
   id = ArrowP id
-  ArrowP g . ArrowP f = ArrowP (g . f)
+
+--(.) :: ArrowP a p b c -> ArrowP a p a1 b -> ArrowP a p a1 c
+  ArrowP f . ArrowP g = ArrowP (f . g)
+
 
 instance Arrow a => Arrow (ArrowP a p) where
+--arr :: (b -> c) -> ArrowP a p b c
   arr f = ArrowP (arr f)
+
+--first :: ArrowP a p b c -> ArrowP a p (b, d) (c, d)
   first (ArrowP f) = ArrowP (first f)
 
+
 instance ArrowLoop a => ArrowLoop (ArrowP a p) where
+--loop :: ArrowP a p (b, d) (c, d) -> ArrowP a p b c
   loop (ArrowP f) = ArrowP (loop f)
 
+
 instance ArrowCircuit a => ArrowCircuit (ArrowP a p) where
-  delay i = ArrowP (delay i)
+--delay :: b -> ArrowP a p b b
+  delay b = ArrowP (delay b)
+
 
 instance ArrowChoice a => ArrowChoice (ArrowP a p) where
+--left :: ArrowP a p b c -> ArrowP a p (Either b d) (Either c d)
   left (ArrowP f) = ArrowP (left f)
-  ArrowP f ||| ArrowP g = ArrowP (f ||| g)
 
+--(|||) :: ArrowP a p b d -> ArrowP a p c d -> ArrowP a p (Either b c) d
+  ArrowP f ||| ArrowP g = ArrowP (f ||| g)
--- Control/SF/SF.hs
+++ Control/SF/SF.hs
@@ -10,74 +10,83 @@ import Control.Category (Category (id, (.)))
 
 import Control.Arrow.ArrowP ()
 
-newtype SF a b = SF { runSF :: (a -> (b, SF a b)) }
+
+-- newtype SF a b = SF { runSF :: a -> (b, SF a b) }
+
+newtype SF a b = SF (a -> (b, SF a b))
+
+runSF :: SF a b -> a -> (b, SF a b)
+runSF (SF x) = x
+
 
 instance Category SF where
-  id = SF h where h x = (x, SF h)
-  g . f = SF (h f g)
-    where
-      h f g x =
-        let (y, f') = runSF f x
-            (z, g') = runSF g y
-        in f' `seq` g' `seq` (z, SF (h f' g'))
+--id :: SF a a
+  id = SF $ \ a -> (a, id)
+
+--(.) :: SF b c -> SF a b -> SF a c
+  SF f . SF g = SF $ \ a ->
+    let (b, sfG') = g a
+        (c, sfF') = f b
+    in  seq sfF' $ seq sfG' (c, sfF' . sfG')
+
 
 instance Arrow SF where
-  arr f = g
-    where g = SF (\x -> (f x, g))
-  first f = SF (g f)
-    where
-      g f (x, z) = f' `seq` ((y, z), SF (g f'))
-        where (y, f') = runSF f x
-  f &&& g = SF (h f g)
-    where
-      h f g x =
-        let (y, f') = runSF f x
-            (z, g') = runSF g x
-        in ((y, z), SF (h f' g'))
-  f *** g = SF (h f g)
-    where
-      h f g x =
-        let (y, f') = runSF f (fst x)
-            (z, g') = runSF g (snd x)
-        in ((y, z), SF (h f' g'))
+--arr :: (b -> c) -> SF b c
+  arr f = SF (\ b -> (f b, arr f))
+
+--first :: SF b c -> SF (b, d) (c, d)
+  first (SF f) = SF $ \ (b, d) ->
+    let (c, sf') = f b
+    in  seq sf' ((c, d), first sf')
+
+--(&&&) :: SF b c -> SF b c' -> SF b (c, c')
+  SF f &&& SF g = SF $ \ b ->
+    let (c,  sfF') = f b
+        (c', sfG') = g b
+    in ((c, c'), sfF' &&& sfG')
+
+--(***) :: SF b c -> SF b' c' -> SF (b, b') (c, c')
+  SF f *** SF g = SF $ \ (b, b') ->
+    let (c,  sfF') = f b
+        (c', sfG') = g b'
+    in ((c, c'), sfF' *** sfG')
+
 
 instance ArrowLoop SF where
-  loop sf = SF (g sf)
-    where
-      g f x = f' `seq` (y, SF (g f'))
-        where ((y, z), f') = runSF f (x, z)
+--loop :: SF (b, d) (c, d) -> SF b c
+  loop (SF f) = SF $ \ b ->
+    let ((c, d), sf') = f (b, d)
+    in  seq sf' (c, loop sf')
+
 
 instance ArrowChoice SF where
-  left sf = SF (g sf)
-    where
-      g f = \case
-        Left a  -> let (y, f') = runSF f a in f' `seq` (Left y, SF (g f'))
-        Right b -> (Right b, SF (g f))
+--left :: SF b c -> SF (Either b d) (Either c d)
+  left sf@(SF f) = SF $ \case
+    Left  b -> seq sf' (Left  c, left sf') where (c, sf') = f b
+    Right d ->         (Right d, left sf )
+
 
 instance ArrowCircuit SF where
-  delay i = SF (f i)
-    where f i x = (i, SF (f x))
+--delay :: b -> SF b b
+  delay b = SF $ \ b' -> (b, delay b')
+
 
 run :: SF a b -> [a] -> [b]
 run _ [] = []
-run (SF f) (x:xs) =
-  let (y, f') = f x
-  in y `seq` f' `seq` (y : run f' xs)
+run (SF f) (a:as) =
+  let (b, f') = f a
+  in  seq b $ seq f' (b : run f' as)
 
 unfold :: SF () a -> [a]
-unfold = flip run inp
-  where inp = () : inp
+unfold = flip run inp where
+  inp = () : inp
 
 
 nth :: Int -> SF () a -> a
-nth n (SF f) = x `seq` if n == 0 then x else nth (n - 1) f'
-  where (x, f') = f ()
+nth n (SF f) = seq a $ if n == 0 then a else nth (n - 1) sf' where
+  (a, sf') = f ()
 
 nth' :: Int -> (b, ((), b) -> (a, b)) -> a
-nth' !n (i, f) = n `seq` i `seq` f `seq` aux n i
-  where
-    aux !n !i = x `seq` i' `seq` if n == 0 then x else aux (n-1) i'
-      where (x, i') = f ((), i)
-
-
-
+nth' !n (b, f) = seq n $ seq b $ seq f $ aux n b where
+  aux !n !b = seq a $ seq b' $ if n == 0 then a else aux (n-1) b' where
+    (a, b') = f ((), b)
